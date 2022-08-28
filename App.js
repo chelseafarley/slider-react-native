0c87f2c46fd54d955fcb79bb08507306378bf580
Chelsea <chelsea@tripwiretech.com>
2022-08-29 10:36:19 +1200

    slider

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Slider from '@react-native-community/slider';
+import { useState } from 'react';
+
+// expo install @react-native-community/slider
 
 export default function App() {
+  const [sliderValue, setSliderValue] = useState("5");
+  const minimumValue = 1;
+  const maximumValue = 10;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput keyboardType='numeric' 
+        style={styles.input} 
+        value={sliderValue} 
+        onChangeText={(value) => setSliderValue(value)} 
+      />
+      <Slider style={{width: 200, height: 40}} 
+        minimumValue={0} 
+        maximumValue={10}
+        onValueChange={(value) => setSliderValue(value)}
+        onSlidingComplete={() => console.log("Sliding complete")}
+        onSlidingStart={() => console.log("Sliding start")}
+        step={0.5}
+        value={sliderValue}
+        maximumTrackTintColor="#ff0000"
+        minimumTrackTintColor="#00ff00"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +40,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    margin: 8,
+    padding: 8,
+    borderColor: "#000000",
+    borderWidth: 1,
+    alignSelf: "stretch"
+  }
 });
